b6e8c27fffe6c9796ce87828067d043b093ac404
milkshake0 <jiwoo992020@gmail.com>
2021-03-13 14:49:36 +0900

    localStorage완료 history미완

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Result = () => {
-  return <div>result</div>;
+const Result = ({ location }) => {
+  console.log("로케이션: ", location);
+  return (
+    <div>
+      result
+      {localStorage.getItem("myAnswer")}
+      그리고
+      {/* {location.state} */}
+    </div>
+  );
 };
 
 export default Result;
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Questions from "./shared/questions.json";
 import Answers from "./shared/answers.json";
 
-const Test = ({ match }) => {
+const Test = ({ match, history }) => {
   const currStep = match.params.step;
-  const history = useHistory();
-  console.log(history);
+  //결과 화면은 if로 step이 10이면 Link to=""에 삼항연산자로 /result/{결과타입} 으로 바꾸기
+  //결과 : 결과 배열에 답변 객체 정리한 것 할당. 이후 결과 객체 sort(기본sort함수넣기)하고 1번 배열 뽑기
+  const [myAnswer, setMyAnswer] = useState(
+    JSON.parse(localStorage.getItem("myAnswer") || "[]")
+  );
 
-  const [myAnswer, setMyAnswer] = useState({});
+  useEffect(() => {
+    localStorage.setItem("myAnswer", JSON.stringify(myAnswer));
+  }, [myAnswer]);
+
   console.log(myAnswer);
   const onClickAnswerA = () => {
     setMyAnswer({
@@ -16,28 +22,39 @@ const Test = ({ match }) => {
       [currStep]: [Questions[currStep]["a1"], Questions[currStep]["a2"]],
     });
     console.log(myAnswer[currStep]);
+    if (currStep >= 10) {
+      history.push({ pathname: "/result" });
+    }
+    history.push({ pathname: `/test/${+currStep + 1}` });
   };
 
   const onClickAnswerB = () => {
     setMyAnswer({ ...myAnswer, [currStep]: null });
     console.log(myAnswer[currStep]);
+
+    if (currStep >= 10) {
+      history.push({ pathname: "/result" });
+    }
   };
+  const clearStorage = () => {
+    setMyAnswer({});
+  };
+  if (currStep >= 11) {
+    history.push({ pathname: "/result" });
+  }
   return (
     <div className="Test">
+      <button onClick={clearStorage}>스토리지 비우기</button>
       <p>{currStep} / 10</p>
-      <h2>{Questions[currStep]["text"]}</h2>
-      <Link
-        onClick={onClickAnswerA}
-        to={currStep >= 10 ? `/test/${+currStep}` : `/test/${+currStep + 1}`}
-      >
-        {Answers[currStep]["a"]}
-      </Link>
-      <Link
-        onClick={onClickAnswerB}
-        to={currStep >= 10 ? `/test/${+currStep}` : `/test/${+currStep + 1}`}
-      >
-        {Answers[currStep]["b"]}
-      </Link>
+      {/* <h2>{Questions[currStep]["text"]}</h2> */}
+      <div onClick={onClickAnswerA}>
+        <Link>{Answers[currStep >= 11 ? 10 : currStep]["a"]}</Link>
+      </div>
+      <div onClick={onClickAnswerB}>
+        <Link to={`/test/${+currStep + 1}`}>
+          {Answers[currStep >= 11 ? 10 : currStep]["b"]}
+        </Link>
+      </div>
     </div>
   );
 };
